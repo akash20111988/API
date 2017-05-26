94dc80afdcd4f2fc55e740c206e97efb09f64c20
Akash <akash20111988@hotmail.com>
2017-05-26 08:50:05 +0530

    weather api and javascript

--- API/Controllers/WeatherController.cs
+++ API/Controllers/WeatherController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,5 +16,21 @@ namespace API.Controllers
         {
             return View();
         }
+
+		public ActionResult WeatherByCity(string id)
+		{
+			var url = "http://api.apixu.com/v1/current.json?key=f06e7c46c24144a5ae4104358170505&q=" + id;
+			//http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=e67fab67a2bc61c221e8a6165965c107
+			var client = new HttpClient();
+
+			client.DefaultRequestHeaders.Accept.Add(
+				new MediaTypeWithQualityHeaderValue("application/json"));
+
+			var response = client.GetAsync(url).Result;
+
+			var weatherModel = response.Content.ReadAsAsync<WeatherModel>().Result;
+			
+			return View(weatherModel);
+		}
     }
 }
